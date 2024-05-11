da92add84ec4daa461f89081ea47d9d12c8c4aaf
Lordsniffer22 <128750702+Lordsniffer22@users.noreply.github.com>
2024-05-11 17:34:30 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget -O run.sh https://raw.githubusercontent.com/Lordsniffer22/fed/main/pydm.py
+wget -O pydm.py https://raw.githubusercontent.com/Lordsniffer22/fed/main/pydm.py
 wget -O .env https://raw.githubusercontent.com/Lordsniffer22/fed/main/.env
 wget -O requirements.txt https://raw.githubusercontent.com/Lordsniffer22/fed/main/requirements.txt
 pip install -r requirements.txt
