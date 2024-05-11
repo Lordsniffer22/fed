96a51299dad6e57f22749f9b86663835ba1d1719
Lordsniffer22 <128750702+Lordsniffer22@users.noreply.github.com>
2024-05-11 14:24:45 +0300

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+wget -O run.sh https://raw.githubusercontent.com/Lordsniffer22/fed/main/pydm.py
+wget -O .env https://raw.githubusercontent.com/Lordsniffer22/fed/main/.env
+pip install requirements.txt
