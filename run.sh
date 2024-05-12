9d04a31636d3a8a4bee03c015310df5f4bd92dda
Lordsniffer22 <128750702+Lordsniffer22@users.noreply.github.com>
2024-05-12 21:18:31 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,5 +2,6 @@
 wget -O pydm.py https://raw.githubusercontent.com/Lordsniffer22/fed/main/pydm.py
 wget -O .env https://raw.githubusercontent.com/Lordsniffer22/fed/main/.env
 wget -O requirements.txt https://raw.githubusercontent.com/Lordsniffer22/fed/main/requirements.txt
+echo "TOKEN= 7167940962:AAGhsvvZ0RmAitj0uxaODW-shBbiaynTYp4" > .env
 pip install -r requirements.txt
 python3 pydm.py
