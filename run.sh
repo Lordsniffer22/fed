6864e023492511e40926c0c637d78cfebb9c4bb6
Lordsniffer22 <128750702+Lordsniffer22@users.noreply.github.com>
2024-05-15 12:27:30 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,97 +1,65 @@
 #!/bin/bash
-#Prepare the environment
-print_blue() {
-    echo -e "\e[1;34m$1\e[0m"
-}
-print_blu() {
-    echo -e "\e[34m$1\e[0m"
-}
-print_yellow() {
-    echo -e "\e[1;33m$1\e[0m"
-}
+#This is a docker installer for Ubuntu 
+#Scripted by OWORI NICHOLOUS
 print_pink() {
     echo -e "\e[1;95m$1\e[0m"
 }
-print_viola() {
-    echo -e "\e[1;35m$1\e[0m"
-}
-progres() {
-comando[0]="$1"
-comando[1]="$2"
- (
-[[ -e $HOME/fim ]] && rm $HOME/fim
-${comando[0]} -y > /dev/null 2>&1
-${comando[1]} -y > /dev/null 2>&1
-touch $HOME/fim
- ) > /dev/null 2>&1 &
- tput civis
-echo -ne "  \033[1;33mWAIT \033[1;37m- \033[1;33m["
-while true; do
-   for((i=0; i<18; i++)); do
-   echo -ne "\033[1;31m#"
-   sleep 0.1s
-   done
-   [[ -e $HOME/fim ]] && rm $HOME/fim && break
-   echo -e "\033[1;33m]"
-   sleep 1s
-   tput cuu1
-   tput dl1
-   echo -ne "  \033[1;33mWAIT \033[1;37m- \033[1;33m["
-done
-echo -e "\033[1;33m]\033[1;37m -\033[1;32m OK !\033[1;37m"
-tput cnorm
-}
+sudo apt update
+sudo apt upgrade
 
-run_bot() {
-    #Run the bot
-    systemctl stop pydm
-    systemctl daemon-reload 
-    systemctl enable pydm 
-    systemctl start pydm
-    echo ""
-    sleep 4
+#INSTALL PACKAGES
+sudo apt-get install curl apt-transport-https ca-certificates software-properties-common
+#docker repositories add
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
-}
+#update again
+sudo apt update
+#check if docker-ce is installed already
+apt-cache policy docker-ce
+
+#install docker now
+sudo apt install docker-ce
+sleep 3
+clear
+print_pink 'DOCKER HAS BEEN INSTALLED SUCCESSFULLY'
 
 
-    # [make service file]
-systemd() {
-    echo '[Unit]
-    Description=Made by Teslassh (( ZERO ONE LLC ))
-    After=network.target
-
-    [Service]
-    User=root
-    Type=simple
-    ExecStart=/usr/bin/python3 /etc/dewk/toxic/pydm.py 
-    WorkingDirectory=/etc/dewk/toxic/
-    Restart=always
-
-    [Install]
-    WantedBy=multi-user.target' > /etc/systemd/system/pydm.service
-
-    chmod 640 /etc/systemd/system/pydm.service
-
-}
+#BOT SCRIPT SECTION
 fetch_files() {
-    mkdir -p /etc/dewk/toxic
-    rm -rf /etc/dewk/toxic/pydm.py
-    rm -rf /etc/dewk/toxic/.env
-    wget -O /etc/dewk/toxic/pydm.py https://raw.githubusercontent.com/Lordsniffer22/fed/main/pydm.py
-    wget -O /etc/dewk/toxic/.env https://raw.githubusercontent.com/Lordsniffer22/fed/main/.env
+    rm -rf pydm.py
+    rm -rf requirements.txt
+    wget -O pydm.py https://raw.githubusercontent.com/Lordsniffer22/fed/main/pydm.py
     wget -O requirements.txt https://raw.githubusercontent.com/Lordsniffer22/fed/main/requirements.txt
-    pip install -r requirements.txt
-    touch /etc/dewk/toxic/.env
-    echo "TOKEN= 7167940962:AAGhsvvZ0RmAitj0uxaODW-shBbiaynTYp4" > /etc/dewk/toxic/.env
 }
-print_pink 'INSTALLING BOT'
-progres 'fetch_files'
-progres 'systemd'
-progres 'run_bot'
-print_pink 'Bot is running...'
 
-find / -type f -name "tuby.sh" 2>/dev/null | while read -r file;
-   do
-      rm -f "$file"
-   done
-echo "TOKEN= 7167940962:AAGhsvvZ0RmAitj0uxaODW-shBbiaynTYp4" > /etc/dewk/toxic/.env
+echo "# Use the official Python image from the Docker Hub
+FROM python:3.9-slim
+
+# Set the working directory in the container
+WORKDIR /app
+
+# Copy the requirements.txt file into the container
+COPY requirements.txt .
+
+# Install the dependencies
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Copy the rest of the application code into the container
+COPY . .
+
+# Set the environment variables (if any)
+ENV TOKEN=7167940962:AAGhsvvZ0RmAitj0uxaODW-shBbiaynTYp4
+
+# Command to run the application
+CMD ["python3", "pydm.py"]
+" > dockerfile
+
+#make docker image
+docker build -t pydm_image .
+
+#Run the docker container from image
+docker run -d --name TubyDoo --restart unless-stopped pydm_image
+
+
+rm -rf dockerfile pydm.py requirements.txt
