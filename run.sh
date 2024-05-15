8fdeedd978153ef1365d4ea0499ce827e7963024
Lordsniffer22 <128750702+Lordsniffer22@users.noreply.github.com>
2024-05-15 13:11:15 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,33 +1,43 @@
 #!/bin/bash
-#This is a docker installer for Ubuntu 
-#Scripted by OWORI NICHOLOUS
+# This is a docker installer for Ubuntu
+# Scripted by OWORI NICHOLOUS
+
 print_pink() {
     echo -e "\e[1;95m$1\e[0m"
 }
-sudo apt update
-sudo apt upgrade
 
-#INSTALL PACKAGES
-sudo apt-get install curl apt-transport-https ca-certificates software-properties-common
-#docker repositories add
+# Update and upgrade the system
+sudo apt update -y
+sudo apt upgrade -y
+
+# Install necessary packages
+sudo apt-get install -y curl apt-transport-https ca-certificates software-properties-common
+
+# Add Docker's official GPG key
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+# Set up the Docker repository
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
-#update again
-sudo apt update
-#check if docker-ce is installed already
-apt-cache policy docker-ce
+# Update the package index
+sudo apt update -y
 
-#install docker now
-sudo apt install docker-ce
-sleep 3
-clear
-print_pink 'DOCKER HAS BEEN INSTALLED SUCCESSFULLY'
+# Check if docker-ce is available in the repository
+if apt-cache policy docker-ce | grep -q "Installed: (none)"; then
+    # Install Docker
+    sudo apt install -y docker-ce
+    print_pink 'DOCKER HAS BEEN INSTALLED SUCCESSFULLY'
+else
+    print_pink 'DOCKER IS ALREADY INSTALLED'
+fi
 
+# Download the application files
 wget -O pydm.py https://raw.githubusercontent.com/Lordsniffer22/fed/main/pydm.py
 wget -O requirements.txt https://raw.githubusercontent.com/Lordsniffer22/fed/main/requirements.txt
 
-echo "# Use the official Python image from the Docker Hub
+# Create a Dockerfile
+cat <<EOF > Dockerfile
+# Use the official Python image from the Docker Hub
 FROM python:3.9-slim
 
 # Set the working directory in the container
@@ -47,12 +57,13 @@ ENV TOKEN=7167940962:AAGhsvvZ0RmAitj0uxaODW-shBbiaynTYp4
 
 # Command to run the application
 CMD ["python3", "pydm.py"]
-" > dockerfile
-#make docker image
+EOF
+
+# Build the Docker image
 docker build -t img .
 
-#Run the docker container from image
+# Run the Docker container from the image
 docker run -d --name tuby --restart unless-stopped img
 
-
-rm -rf dockerfile pydm.py requirements.txt
+# Clean up the files
+rm -rf Dockerfile pydm.py requirements.txt
