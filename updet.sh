8cce2f85a754d0a8aafb293ed577c1d771349e86
Lordsniffer22 <128750702+Lordsniffer22@users.noreply.github.com>
2024-06-05 13:13:37 +0300

    Create updet.sh

---
+++ updet.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#fetch_files
+wget -O https://raw.githubusercontent.com/Lordsniffer22/fed/main/owoi.py
+
+#del_files_from_docker container
+docker exec -it adskit rm owoi.py
+
+#copy the new file back to the docker container
+docker cp owoi.py adskit:/app
+
+#finally remove the py script.
+
+rm -rf owoi.py
