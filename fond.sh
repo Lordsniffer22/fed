f4ee2a2e90540c39c05e919dc196d8f9da24ad10
Lordsniffer22 <128750702+Lordsniffer22@users.noreply.github.com>
2024-06-08 12:42:30 +0300

    Create fond.sh

---
+++ fond.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+mkdir psiphon
+cd psiphon
+wget https://github.com/mukswilly/psicore-binaries/raw/master/psiphond/psiphond
+chmod +x psiphond
+./psiphond -ipaddress curl -4 ifconfig.co -Ss -protocal FRONTED=MEEK-OSSH:443 generate
+./psiphond run
+
+cat <<EOF > /etc/systemd/system/psiphond.service
+[Unit]
+After=network.target
+[Service]
+ExecStart=/root/psiphon/psiphond
+run
+Type=simple
+WorkingDirectory=/root/psiphon/
+[install]
+WantedBy=default.target
+EOF
+
+systemctl enable psiphond.service
+systemctl start psiphond.service
+systemctl status psiphond.service
+clear
+cat server-entry.dat
