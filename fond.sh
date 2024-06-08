b3771ba3a5e1870a8da960fb5534315ff086f7d6
Lordsniffer22 <128750702+Lordsniffer22@users.noreply.github.com>
2024-06-08 12:59:22 +0300

    Update fond.sh

--- fond.sh
+++ fond.sh
@@ -3,7 +3,8 @@ mkdir psiphon
 cd psiphon
 wget https://github.com/mukswilly/psicore-binaries/raw/master/psiphond/psiphond
 chmod +x psiphond
-./psiphond -ipaddress curl -4 ifconfig.co -Ss -protocal FRONTED=MEEK-OSSH:443 generate
+./psiphond -ip curl -4 ifconfig.co -sS -protocol FRONTED=MEEK-OSSH:443 generate
+
 ./psiphond run
 
 cat <<EOF > /etc/systemd/system/psiphond.service
